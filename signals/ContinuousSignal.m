fd52b9a19107c82957cf1db2a27906fba0a51ad7
Bowen <bowenbhwu@gmail.com>
2022-04-18 02:42:58 +0800

    Add: ContinuousSignal implementation

--- signals/ContinuousSignal.m
+++ signals/ContinuousSignal.m
@@ -1,22 +1,47 @@
-classdef ContinuousSignal
-    %CONTINUOUSSIGNAL Summary of this class goes here
-    %   Detailed explanation goes here
+classdef ContinuousSignal < Signal
+    % ContinuousSignals are signals that outputs continous values.
+    % Lookup Name:
+    %   Signal.ContinuousSignal
     
     properties
-        Property1
+        lowerBound
+        upperBound
+        valueType
     end
     
     methods
-        function obj = ContinuousSignal(inputArg1,inputArg2)
-            %CONTINUOUSSIGNAL Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
+        function node = ContinuousSignal(lowerBound, upperBound, returnType)
+            node = node@Signal();
+            node.appendLookupName('ContinuousSignal');
+            if strcmp(returnType, 'int')
+                node.valueType = 1;
+            elseif strcmp(returnType, 'double')
+                node.valueType = 2;
+            else
+                error("Unknown valuesType.");
+            end
+            node.lowerBound = lowerBound;
+            node.upperBound = upperBound;
         end
         
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+        function init(node)
+            if node.valueType == 1
+                node.signal = randi([node.lowerBound, node.upperBound]);
+            elseif node.valueType == 2
+                node.signal = node.lowerBound + ...
+                    (node.upperBound - node.lowerBound) * rand();
+            end
+        end
+        
+        function mutate(node, options, ~)
+            if rand() < options.terminalMutationRate
+                if node.valueType == 1
+                    node.signal = randi([node.lowerBound, node.upperBound]);
+                else
+                    node.signal = node.lowerBound + ...
+                        (node.upperBound - node.lowerBound) * rand();
+                end
+            end
         end
     end
 end
